ec9bdd8f37f4e4c15d5bda45a14b6b76b6588641
Ndoxi <90957973+Ndoxi@users.noreply.github.com>
2025-11-29 19:46:39 +0200

    feat: better movement and collision detection

--- Assets/Scripts/Gameplay/Entities/States/MoveState.cs
+++ Assets/Scripts/Gameplay/Entities/States/MoveState.cs
@@ -13,6 +13,7 @@ namespace TowerDefence.Gameplay.States
         private readonly ICommandCenter _commandCenter;
         private readonly IAttackSystem _attackSystem;
         private readonly Stat _moveSpeedStat;
+        private readonly ITickDispatcher _tickDispatcher;
         private Vector2 _direction;
 
         public MoveState(IEntity entity,
@@ -25,6 +26,7 @@ namespace TowerDefence.Gameplay.States
             _commandCenter = commandCenter;
             _attackSystem = attackSystem;
             _moveSpeedStat = _entity.GetStat(StatType.MoveSpeed);
+            _tickDispatcher = Services.Get<ITickDispatcher>();
         }
 
         public void OnEnter(IStateContext context = null) 
@@ -32,23 +34,36 @@ namespace TowerDefence.Gameplay.States
             if (context is CommandStateContext<MoveCommand> commandContext)
                 _direction = commandContext.command.move;
 
+            _tickDispatcher.Subscribe(HandleMovement, TickType.FixedUpdate);
+
             _commandCenter.Subscribe<MoveCommand>(OnMoveCommand);
             _commandCenter.Subscribe<AttackCommand>(OnAttackCommand);
         }
 
         public void OnExit() 
         {
+            _tickDispatcher.Unsubscribe(HandleMovement, TickType.FixedUpdate);
+
             _commandCenter.Unsubscribe<MoveCommand>(OnMoveCommand);
             _commandCenter.Unsubscribe<AttackCommand>(OnAttackCommand);
         }
 
-        public void Tick(float deltaTime) 
-        {
-            Vector2 movementVector2D = _moveSpeedStat.value * deltaTime * _direction;
-            Vector3 movementVector = new (movementVector2D.x, 0f, movementVector2D.y);
-            _rigidbody.MovePosition(_rigidbody.position + movementVector);
+        public void Tick(float deltaTime) { }
 
-            if (movementVector != Vector3.zero)
+        private void HandleMovement(float deltaTime)
+        {
+            if (_direction.sqrMagnitude < 0.01f)
+            {
+                _rigidbody.linearVelocity = Vector2.zero;
+                _entity.SetState<IdleState>();
+                return;
+            }
+
+            Vector2 movementVector2D = _moveSpeedStat.value * _direction;
+            var movementVector = new Vector3(movementVector2D.x, 0f, movementVector2D.y);
+            _rigidbody.linearVelocity = movementVector;
+
+            if (_direction.sqrMagnitude > 0.01f)
             {
                 Vector3 forward = movementVector.normalized;
                 _rigidbody.MoveRotation(Quaternion.LookRotation(forward));
@@ -58,9 +73,6 @@ namespace TowerDefence.Gameplay.States
         private void OnMoveCommand(MoveCommand moveCommand)
         {
             _direction = moveCommand.move;
-
-            if (_direction == Vector2.zero)
-                _entity.SetState<IdleState>();
         }
 
         private void OnAttackCommand(AttackCommand attackCommand)
